be4d43d98767ebe8de03cc967360d4293b4769fa
Harish Mohan Raj <harishuideveloper@outlook.com>
2016-03-11 19:39:14 +0530

    basic working model

--- js/components/ArrowComponent.js
+++ js/components/ArrowComponent.js
@@ -1,29 +1,34 @@
 var React = require('react');
+var CarouselActions = require('../actions/CarouselActions');
 
 var Arrow = React.createClass({
+    slideNext : function(activeIndex,length){
+        CarouselActions.nextClicked(activeIndex,length);
+    },
+    slidePrev : function(activeIndex,length){
+        CarouselActions.prevClicked(activeIndex,length);
+    },
     returnButton : function(){
         var returnButtonItem,
             direction = this.props.arrowType,
             notFirstElement = !!this.props.arrowPropsObject.activeIndex,
-            notLastElement = this.props.arrowPropsObject.activeIndex !== this.props.arrowPropsObject.length -1,
-            infiniteScroll = this.props.arrowPropsObject.infiniteScroll;
-            
-        if(this.props.arrowPropsObject.showArrows){
+            notLastElement = this.props.arrowPropsObject.activeIndex !== this.props.arrowPropsObject.length -1;
+
             if(direction==='left'){
-                if(infiniteScroll || notFirstElement){
+                if(notFirstElement){
                     returnButtonItem =  (
-                        <a className="prev" onClick={this.props.slidePrev}><i className="material-icons md-icon dp48">keyboard_arrow_left</i></a>
+                        <a className="prev" onClick={this.slidePrev.bind(this,this.props.arrowPropsObject.activeIndex,this.props.arrowPropsObject.length)}><i className="material-icons md-icon dp48">keyboard_arrow_left</i></a>
                     )
                 }
 
             }else{
-                if(infiniteScroll || notLastElement){
+                if(notLastElement){
                     returnButtonItem = (
-                        <a className="next" onClick={this.props.slideNext}><i className="material-icons md-icon dp48">keyboard_arrow_right</i></a>
+                        <a className="next" onClick={this.slideNext.bind(this,this.props.arrowPropsObject.activeIndex,this.props.arrowPropsObject.length)}><i className="material-icons md-icon dp48">keyboard_arrow_right</i></a>
                     )
                 }
             }
-        }
+
         return returnButtonItem;
     },
     render : function(){
